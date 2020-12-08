65b158768ef40780612d84642f7a9b07a36b7123
Lauri Lukkarinen <lauri.lukkarinen@metatavu.fi>
2020-12-08 15:00:26 +0200

    Added styles

---
+++ custom-styles/generic/confirm-dialog.ts
@@ -0,0 +1,15 @@
+import { createStyles } from "@material-ui/core";
+import theme from "../../theme";
+
+export default createStyles({
+
+  termsOfUseLink: {
+    color: theme.palette.primary.contrastText
+  },
+
+  confirmButton: {
+    color: theme.palette.common.white,
+    backgroundColor: theme.palette.primary.main
+  },
+
+});
--- custom-styles/generic/header.ts
+++ custom-styles/generic/header.ts
@@ -13,7 +13,7 @@ export default createStyles({
 
   logo: { },
 
-  select: { },
+  selectRoot: { },
 
   menuItem: { },
 
